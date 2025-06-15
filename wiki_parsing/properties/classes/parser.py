94bf6f14d2c4be743dae980932af836f0d3104cf
ocet247 <ocet247@gmail.com>
2025-06-15 21:14:21 +0200

    Now the netprop list should be complete

--- wiki_parsing/properties/classes/parser.py
+++ wiki_parsing/properties/classes/parser.py
@@ -1,18 +1,15 @@
 import re
+from sortedcontainers import SortedSet
 
 with open("input.txt", "r") as input, open("output.txt", "w") as output:
-    classes = {}
+    set = SortedSet()
+
     for line in input:
-        class_match = re.match(r"(\w+) - (\w+)", line)
+        class_match = re.match(r"\w+ - (\w+)", line)
         if not class_match:
             continue
 
-        cpp_class = class_match.group(1)
-        if cpp_class in classes:
-            continue
+        set.add(class_match.group(1))
 
-        classname = class_match.group(2)
-        classes[cpp_class] = classname
-
-    for classname in classes.values():
+    for classname in set:
         output.write(f'\t"{classname}",\n')
